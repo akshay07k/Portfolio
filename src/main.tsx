a8f00136a8b0b2e74d6b27280dfb95da49de611b
akshay07k <akshaykumar5651t@gmail.com>
2024-09-05 13:56:42 +0530

    setup routers

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { HomePage, LifelinkPage } from './pages'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />
+      },
+      {
+        path: '/lifelink',
+        element: <LifelinkPage />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
