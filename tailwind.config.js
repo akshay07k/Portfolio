94a1b6f1f97bbb82c25abad8d52c044a4952af27
akshay07k <akshaykumar5651t@gmail.com>
2024-09-05 13:42:59 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+      },
+    },
+  },
+  plugins: [],
+}
+
