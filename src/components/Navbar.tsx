25933dcbe7ef709182442493de94a5da1d4a8ed7
akshay07k <154620666+akshay07k@users.noreply.github.com>
2025-10-07 20:41:04 +0530

    Updated resume link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,7 +62,7 @@ const Navbar: React.FC = () => {
                 </ul>
             </div>
             <div>
-                <a href='https://drive.google.com/file/d/1VhmL7Jz1abSMbNekofdMRzaF57JMjoI_/view'>
+                <a href='https://drive.google.com/file/d/1h6DHakRzX060gKZ8DTk1QcxcXaAWAj3b/view?usp=sharing'>
                 <button className="hidden sm:flex className='mt-8 w-full justify-center py-2 px-4 text-sm rounded-lg bg-gradient-to-r from-[#306279] to-[#5791b5] text-white uppercase shadow-xl transition hover:scale-105 duration-300 ease-in">Resume</button>    
                 </a>
             </div>
